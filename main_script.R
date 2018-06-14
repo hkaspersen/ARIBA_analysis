020f27cd7f89c45158b5fb652a53096ae19ef0a5
hkaspersen <hakon.kaspersen@protonmail.com>
2018-06-14 13:35:44 +0200

    updated script for plot heatmap plot

--- main_script.R
+++ main_script.R
@@ -5,6 +5,10 @@ library(tidyverse)
 # Collapses data frame to unique lines while ignoring NA's
 func_paste <- function(x) paste(unique(x[!is.na(x)]), collapse = ", ")
 
+# Confidence interval function
+get_binCI <- function(x, n) as.numeric(setNames(binom.test(x,n)$conf.int*100,
+                                                c("lwr", "upr")))
+
 # Identifies folder names for extracting report.tsv
 folder_names <- function(filepath,pattern) {
   folders <- list.files(path = filepath, pattern = paste0("_",pattern))
@@ -165,10 +169,74 @@ gene_report_df <- gene_stat_df_complete %>%
   mutate_at(.vars = vars(-ref),
             funs(as.numeric(as.character(.)))) %>%
   mutate(total = ncol(.)-1,
-         no_samples_mut = rowSums(.[,2:11]),
-         percent_genes = no_samples_mut/total *100)
+         no_samples = rowSums(.[,2:11]),
+         percent_genes = no_samples/total *100) %>%
+  rowwise() %>%
+  mutate(lwr = get_binCI(no_samples, total)[1],
+         upr = get_binCI(no_samples, total)[2])
 
 
+ggplot(gene_report_df, aes(ref, percent_genes)) +
+  geom_col() +
+  geom_errorbar(aes(ymin = lwr, ymax = upr),
+                alpha = 0.3,
+                width = 0.4)+
+  labs(y = "Percent of Samples")+
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
+
+# -------------------------------------------------------------------------------------
+## Testing for presence of mutations in genes only
+
+heatmap_df <- mut_data %>%
+  select(ref, cluster, flag, ctg_cov, ref_ctg_change) %>%
+  mutate(id = 1:n()) %>%
+  spread(cluster, ref_ctg_change) %>%
+  group_by(ref) %>%
+  summarise_all(funs(func_paste)) %>%
+  mutate(ref = gsub("^(.*?)_(.*?)$", "\\1", ref)) %>%
+  select(-c(id, flag, ctg_cov)) %>%
+  gather(gene, mut,-ref) %>%
+  group_by(gene) %>%
+  filter(!any(mut == "")) %>%
+  ungroup() %>%
+  mutate(mut = ifelse(mut == ".", NA, mut)) %>%
+  separate(mut, into = as.character(c(1:35))) %>%
+  gather(no_mut, mut,-ref,-gene) %>%
+  mutate(mut = ifelse(mut == "", NA, ifelse(!is.na(mut), 1, mut)),
+         mut = as.numeric(mut)) %>%
+  spread(no_mut, mut) %>%
+  mutate(sum = rowSums(subset(., select = -c(ref, gene)), na.rm = TRUE)) %>%
+  select(ref, gene, sum)
+
+ggplot(heatmap_df, aes(gene, ref, fill = sum)) +
+  geom_tile(color = "white") +
+  labs(fill = "Mutations") +
+  scale_fill_distiller(palette = 1)+
+  theme(
+    axis.text.x = element_text(
+      angle = 90,
+      size = 9,
+      hjust = 1,
+      vjust = 0.3
+    ),
+    axis.text.y = element_text(size = 9),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank()
+  ) +
+  coord_fixed()
+
+ggsave(
+  "heamap.tiff",
+  p,
+  device = "tiff",
+  units = "cm",
+  dpi = 300,
+  width = 40,
+  height = 30
+)
+
+?scale_fill_distiller
 
 
 
